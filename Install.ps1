e464ac232e1b92f3e1c7d28464837fee7043142f
Václav Lipert <vlipert@seznam.cz>
2022-07-16 00:14:22 +0200

    Formatting + switch from tabs to spaces

--- Install.ps1
+++ Install.ps1
@@ -1,33 +1,31 @@
-If([IntPtr]::Size -ne 8)
-{
+If ([IntPtr]::Size -ne 8) {
     Write-Host "Script is not running as 64bit!!"
     Exit
 }
 
 $ver = $host | select version
-if ($ver.Version.Major -gt 1)  {$Host.Runspace.ThreadOptions = "ReuseThread"}
+if ($ver.Version.Major -gt 1) { $Host.Runspace.ThreadOptions = "ReuseThread" }
 
 # Verify that user running script is an administrator
-$IsAdmin=[Security.Principal.WindowsIdentity]::GetCurrent()
-If ((New-Object Security.Principal.WindowsPrincipal $IsAdmin).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator) -eq $FALSE)
-{
-  "`nERROR: You are NOT a local administrator.  Run this script after logging on with a local administrator account."
-	# We are not running "as Administrator" - so relaunch as administrator
+$IsAdmin = [Security.Principal.WindowsIdentity]::GetCurrent()
+If ((New-Object Security.Principal.WindowsPrincipal $IsAdmin).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator) -eq $FALSE) {
+    "`nERROR: You are NOT a local administrator.  Run this script after logging on with a local administrator account."
+    # We are not running "as Administrator" - so relaunch as administrator
 
-	# Create a new process object that starts PowerShell
-	$newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+    # Create a new process object that starts PowerShell
+    $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
 
-	# Specify the current script path and name as a parameter
-	$newProcess.Arguments = $myInvocation.MyCommand.Definition;
+    # Specify the current script path and name as a parameter
+    $newProcess.Arguments = $myInvocation.MyCommand.Definition;
 
-	# Indicate that the process should be elevated
-	$newProcess.Verb = "runas";
+    # Indicate that the process should be elevated
+    $newProcess.Verb = "runas";
 
-	# Start the new process
-	[System.Diagnostics.Process]::Start($newProcess);
+    # Start the new process
+    [System.Diagnostics.Process]::Start($newProcess);
 
-	# Exit from the current, unelevated, process
-	exit
+    # Exit from the current, unelevated, process
+    exit
 }
 
 Set-Location $PSScriptRoot
@@ -35,51 +33,44 @@ Set-Location $PSScriptRoot
 Write-Host "Saitek Force Feedback driver fix (https://github.com/WallyCZ/saitek-cyborg-ff)"
 
 $targets = @(
-	@{ DllName="SaiQFFB5.dll"; DeviceName="Saitek Cyborg Evo Force"; HardwareId="VID_06A3&PID_FFB5"; RenamedDevice="Saitek Cyborg Evo FF" },
-	@{ DllName="SaiQFF12.dll"; DeviceName="Saitek Cyborg 3D Rumble Force"; HardwareId="VID_06A3&PID_FF52"; RenamedDevice="Saitek Cyborg 3D Rumble FF"}
-    @{ DllName="SaiQFF12.dll"; DeviceName="Saitek Cyborg 3D Force Stick"; HardwareId="VID_06A3&PID_FF12"; RenamedDevice="Saitek Cyborg 3D FS" }
-    )
+    @{ DllName = "SaiQFFB5.dll"; DeviceName = "Saitek Cyborg Evo Force"; HardwareId = "VID_06A3&PID_FFB5"; RenamedDevice = "Saitek Cyborg Evo FF" },
+    @{ DllName = "SaiQFF12.dll"; DeviceName = "Saitek Cyborg 3D Rumble Force"; HardwareId = "VID_06A3&PID_FF52"; RenamedDevice = "Saitek Cyborg 3D Rumble FF" },
+    @{ DllName = "SaiQFF12.dll"; DeviceName = "Saitek Cyborg 3D Force Stick"; HardwareId = "VID_06A3&PID_FF12"; RenamedDevice = "Saitek Cyborg 3D FS" }
+)
 
 $found = $false
 
-foreach ($target in $targets)
-{
-  $targetpath = ([System.Environment]::SystemDirectory + "\\" + $target.DllName)
+foreach ($target in $targets) {
+    $targetpath = ([System.Environment]::SystemDirectory + "\\" + $target.DllName)
   
-  if (Test-Path -Path $targetpath -PathType Leaf)
-  {
-	Write-Host "Replacing $($target.DeviceName) FF driver with version $((Get-Item SaiQFFB5.dll).VersionInfo.FileVersion)"
-	Try
-	{
-		Copy-Item -Path "SaiQFFB5.dll" -Destination $targetpath -force
-	}
-	Catch
-	{
-		Rename-Item -Path $targetpath -NewName ($target.DllName + ".bak")
-		Copy-Item -Path "SaiQFFB5.dll" -Destination $targetpath -force
-	}
+    if (Test-Path -Path $targetpath -PathType Leaf) {
+        Write-Host "Replacing $($target.DeviceName) FF driver with version $((Get-Item SaiQFFB5.dll).VersionInfo.FileVersion)"
+        Try {
+            Copy-Item -Path "SaiQFFB5.dll" -Destination $targetpath -force
+        }
+        Catch {
+            Rename-Item -Path $targetpath -NewName ($target.DllName + ".bak")
+            Copy-Item -Path "SaiQFFB5.dll" -Destination $targetpath -force
+        }
 	
-	$confirmation = Read-Host "Do you want to change device name? (needed for some games like DCS) [Y/n]"
-	if (($confirmation -eq 'y') -or ($confirmation -eq 'Y') -or ($confirmation -eq '')) {
+        $confirmation = Read-Host "Do you want to change device name? (needed for some games like DCS) [Y/n]"
+        if (($confirmation -eq 'y') -or ($confirmation -eq 'Y') -or ($confirmation -eq '')) {
         
-         $RegistryPath = "HKCU:\System\CurrentControlSet\Control\MediaProperties\PrivateProperties\Joystick\OEM\$($target.HardwareId)"
-		 $Name         = 'OEMName'
+            $RegistryPath = "HKCU:\System\CurrentControlSet\Control\MediaProperties\PrivateProperties\Joystick\OEM\$($target.HardwareId)"
+            $Name = 'OEMName'
 
-        try
-        {
-		    New-ItemProperty -Path $RegistryPath -Name $Name -Value $target.RenamedDevice -PropertyType String -Force -ErrorAction Stop
+            try {
+                New-ItemProperty -Path $RegistryPath -Name $Name -Value $target.RenamedDevice -PropertyType String -Force -ErrorAction Stop
+            }
+            catch {
+                Write-Host "Device $($target.DeviceName) not found in registry, skipping"
+            }
         }
-        catch
-        {
-            Write-Host "Device $($target.DeviceName) not found in registry, skipping"
-        }
-	}
-	$found = $true
-  }
+        $found = $true
+    }
 }
 
 
-if(-not $found)
-{
-	Write-Host "No Saitek FF driver found, please install it before running this script"
+if (-not $found) {
+    Write-Host "No Saitek FF driver found, please install it before running this script"
 }
